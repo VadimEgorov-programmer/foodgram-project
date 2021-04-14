e27fbfb49de3dcf556740225cfc559376e265a4a
Vadim <wadim-egoroff@yandex.ru>
2021-04-14 13:40:45 +0300

    logging of the debug level

--- recipes/management/commands/load_data.py
+++ recipes/management/commands/load_data.py
@@ -1,14 +1,20 @@
 from django.core.management.base import BaseCommand, CommandError
 from recipes.models import Ingredient
 import csv
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        # Подключать для логирования чтобы не было ошибки
+        # file was loaded in the wrong encoding: 'UTF-8'
+        # with open('recipes/data/ingredients.csv') as file:
         with open('recipes/data/ingredients.csv', encoding='utf-8') as file:
             file_reader = csv.reader(file)
             for row in file_reader:
                 title, dimension = row
                 Ingredient.objects.get_or_create(title=title,
                                                  dimension=dimension)
-                print(title + ',' + dimension)
+                # logger.debug(row)  # Логирование
