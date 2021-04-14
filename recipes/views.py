a75f95c3a2c73d5fd662c01e727216d13260a3ee
Vadim <wadim-egoroff@yandex.ru>
2021-04-14 16:44:07 +0300

    404, 500

--- recipes/views.py
+++ recipes/views.py
@@ -151,12 +151,3 @@ class DownloadPurchasesListView(View):
             as_attachment=True,
             filename='purchases.pdf',
         )
-
-
-def page_not_found(request, exception):
-    return render(
-        request, 'misc/404.html', {'path': request.path}, status=404)
-
-
-def server_error(request):
-    return render(request, 'misc/500.html', status=500)
