f612342ab792800e43ddc9d006c9bd650ae53802
Vadim <wadim-egoroff@yandex.ru>
2021-04-15 15:13:36 +0300

    docker

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -34,7 +34,12 @@ DEBUG = False
 
 AUTH_USER_MODEL = 'users.User'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'web',
+    '178.154.215.227',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
