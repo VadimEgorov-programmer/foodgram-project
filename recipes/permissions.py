bb4a198eb0f865398e45529ba0ed3af1b422e41b
Vadim <wadim-egoroff@yandex.ru>
2021-04-18 16:24:04 +0300

    Two admin checks?

--- recipes/permissions.py
+++ recipes/permissions.py
@@ -3,8 +3,7 @@ from django.shortcuts import redirect
 
 class AdminAuthorPermission:
     def dispatch(self, request, *args, **kwargs):
-        if (self.request.user.is_admin
-                or self.request.user.is_admin
+        if (self.self.request.user.is_admin
                 or self.request.user.username == self.kwargs['username']):
             return super().dispatch(request, *args, **kwargs)
         return redirect(
