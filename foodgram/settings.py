336ca683db46075e4110b921d8a5190d3e91fc3c
Vadim <wadim-egoroff@yandex.ru>
2021-05-27 11:02:29 +0300

    fix safety

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -133,7 +133,7 @@ SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'foodgram.vadimegorov.programmer@gmail.com'
-EMAIL_HOST_PASSWORD = 'u3kFt6ruKPPQrQDQtcNSWbwJ5K5z'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
